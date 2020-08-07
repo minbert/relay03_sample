2979f4776abcb10b8fdbd1c04b708c857ba4b1d0
seokminKang <soos0228@naver.com>
2020-08-07 16:54:23 +0900

    first

---
+++ src/NavigationBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  menu: {
+    flexGrow: 1,
+  },
+  empty: {
+    flexGrow: 10,
+  }
+}));
+
+export default function NavigationBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            부캠 나우
+          </Typography>
+          <Button color="inherit" className={classes.menu}>자유게시판</Button>
+          <Button color="inherit" className={classes.menu}>동물</Button>
+          <Button color="inherit" className={classes.menu}>인물</Button>
+          <Button color="inherit" className={classes.menu}>운동</Button>
+          <div className={classes.empty}></div>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
---
+++ src/Table.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+const columns = [
+  { id: 'number', label: 'No.', minWidth: 10 },
+  { id: 'title', label: 'Title', minWidth: 200 },
+  {
+    id: 'author',
+    label: 'Author',
+    minWidth: 20,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'date',
+    label: 'Date',
+    minWidth: 30,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+];
+
+function createData(number, title, author, date) {
+  return { number, title, author, date };
+}
+
+const rows = [
+  createData(1 , '부스트캠프 스타트', '관리자', '20-08-07'),
+  createData(2 , '부스트캠프 스타트', '관리자', '20-08-07'),
+  createData(3 , '부스트캠프 스타트', '관리자', '20-08-07'),
+  createData(4 , '부스트캠프 스타트', '관리자', '20-08-07'),
+  createData(5 , '부스트캠프 스타트', '관리자', '20-08-07'),
+  createData(6 , '부스트캠프 스타트', '관리자', '20-08-07'),
+  createData(7 , '부스트캠프 스타트', '관리자', '20-08-07'),
+  createData(8 , '부스트캠프 스타트', '관리자', '20-08-07'),
+  createData(9 , '부스트캠프 스타트', '관리자', '20-08-07'),
+  createData(10 , '부스트캠프 스타트', '관리자', '20-08-07'),
+];
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+});
+
+export default function StickyHeadTable() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
